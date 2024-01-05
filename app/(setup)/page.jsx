234355caa615c24c0b96d62b6a9b8291e0bb8e2b
Trijay1709 <trijaypatel1709@gmail.com>
2024-01-05 14:05:05 +0530

    second iteration

---
+++ app/(setup)/page.jsx
@@ -0,0 +1,29 @@
+// "use client"
+import React from 'react'
+import { initialProfile } from '@/lib/initial-profile'
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation';
+import InitialModal from '@/components/modals/Initial-modal';
+// import { useForm } from 'react-hook-form';
+const Setup = async () => {
+  const profile =await initialProfile();
+  const server = await db.server.findFirst({
+    where:{
+        members:{
+            some:{
+                profileId: profile.id
+            }
+        }
+    }
+  });
+  if(server){
+    return redirect(`/servers/${server.id}`);
+  }else{
+
+    return (
+      <InitialModal/>
+    
+    )
+  }
+}
+export default Setup;
